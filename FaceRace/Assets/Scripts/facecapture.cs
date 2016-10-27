b77b815b91d7fd5a2e02948a27e4cae7fcf6f396
Sebastian Rivera <Sebastian Rivera>
2016-10-26 22:56:00 -0400

    First Commit

---
+++ FaceRace/Assets/Scripts/facecapture.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class facecapture : MonoBehaviour {
+
+    // Use this for initialization
+
+    //These names can be changed for facial expressions
+    public WebCamTexture mFace;
+
+    //planes for each expression
+    public GameObject plane1;
+    public GameObject plane2;
+    public GameObject plane3;
+    public GameObject plane4;
+    public GameObject plane5;
+    public GameObject plane6;
+    public GameObject plane7;
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void setHappyFace(WebCamTexture texture)
+    {
+
+    }
+}
